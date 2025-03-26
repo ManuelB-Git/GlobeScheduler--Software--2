a749b50aef6583043385f63c87715838b116850a
Juan Manuel Benitez <jm_benitez@outlook.com>
2025-03-26 08:55:31 -0500

    Refactor UI components and add time population feature
    - Updated `GlobeMessageBox.xaml` for improved styling and alignment.
    - Modified `MainWindow.xaml` to standardize the title and icon.
    - Enhanced `Appointments.xaml` with a responsive layout and data display.
    - Integrated `TimePopulate` helper in `Appointments` and `DashBoard` for time selection.
    - Expanded `ComboBox` options in `DashBoard.xaml` and `Reports.xaml`.
    - Created `TimePopulate.cs` to streamline time entry in `ComboBox`.

---
+++ Helpers/TimePopulate.cs
@@ -0,0 +1,19 @@
+﻿
+namespace GlobeScheduler___Software_2.Helpers
+{
+    public static class TimePopulate
+    {
+        public static void Populate(System.Windows.Controls.ComboBox comboBox)
+        {
+            for (int hour = 0; hour < 24; hour++)
+            {
+                for (int minute = 0; minute < 60; minute += 30)
+                {
+                    DateTime time = new(1, 1, 1, hour, minute, 0);
+                    string timeString = time.ToString("hh:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+                    comboBox.Items.Add(timeString);
+                }
+            }
+        }
+    }
+}
